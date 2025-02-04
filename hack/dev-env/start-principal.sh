1cbcc5c28425efe063f5a381e2b0f9d2ea87f568
Jayendra Parsai <jparsai@jparsai-thinkpadp1gen4i.remote.csb>
2025-02-04 18:26:07 +0530

    feat: support periodic ping to principal for keeping connection alive
    Signed-off-by: Jayendra Parsai <jparsai@jparsai-thinkpadp1gen4i.remote.csb>

--- hack/dev-env/start-principal.sh
+++ hack/dev-env/start-principal.sh
@@ -27,7 +27,8 @@ go run github.com/argoproj-labs/argocd-agent/cmd/principal \
 	--kubecontext vcluster-control-plane \
 	--log-level trace \
 	--namespace argocd \
-	--passwd ${SCRIPTPATH}/creds/users.control-plane $ARGS
+	--passwd ${SCRIPTPATH}/creds/users.control-plane $ARGS \
+    #--wait-duration-for-agent-ping 10m
 	#--resource-proxy-ca-path ${SCRIPTPATH}/creds/ca.crt \
 	#--resource-proxy-cert-path ${SCRIPTPATH}/creds/rp.crt \
 	#--resource-proxy-key-path ${SCRIPTPATH}/creds/rp.key \
